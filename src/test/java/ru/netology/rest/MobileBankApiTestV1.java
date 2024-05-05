1400fad5486c0d84a8239ce470641d6414bc210f
MailMan <ainddal@gmail.com>
2024-05-05 10:09:23 +0300

    Failed test

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -10,7 +10,7 @@ class MobileBankApiTestV1 {
       // Given - When - Then
       // Предусловия
       given()
-          .baseUri("http://localhost:9999/api/v1")
+          .baseUri("http://localhost:9999/api/v2")
       // Выполняемые действия
       .when()
           .get("/demo/accounts")
