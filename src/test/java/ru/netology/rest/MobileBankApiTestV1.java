057f81f4a4f87fada115261b966522db50a85b24
MailMan <ainddal@gmail.com>
2024-05-05 10:14:51 +0300

    correct test

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -10,7 +10,7 @@ class MobileBankApiTestV1 {
       // Given - When - Then
       // Предусловия
       given()
-          .baseUri("http://localhost:9999/api/v22")
+          .baseUri("http://localhost:9999/api/v1")
       // Выполняемые действия
       .when()
           .get("/demo/accounts")
